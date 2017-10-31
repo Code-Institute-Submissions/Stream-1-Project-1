b1856f271aa9eba3af86d43736cafa4ac5045952
sarah barron <sarahflavin@yahoo.com>
2017-10-31 01:10:58 +0000

    link to news & event page

--- js/news&events.js
+++ js/news&events.js
@@ -4,13 +4,12 @@
 //This is an array of news and events:
 //title is the news title
 //image is the link to the image for the news piece
-//link is the link to the main article on the news & events webpage.
 //text is a small snippet of text explaining the news or event.
-var news = [{title:"Cake Sale", image:'"pictures/news/cakesaleindex.jpg"', link:'"NewsAndEvents.html#news1"',
+var news = [{title:"Cake Sale", image:'"pictures/news/cakesaleindex.jpg"',
 text:"our annual cakesale will take place Jan 12th December at 12 am, please come along and support"},
-{title:"School Mass", image:'"pictures/news/handsprayingindex.jpg"',link:'"NewsAndEvents.html#news3"', 
+{title:"School Mass", image:'"pictures/news/handsprayingindex.jpg"',
 text:"Our School Mass will take place on 10th October at 10am, all parents are welcome"},
-{title:"Credit Union", image:'"pictures/CREDITUNION/creditunionindex.jpg"', link:'"NewsAndEvents.html#news2"',
+{title:"Credit Union", image:'"pictures/CREDITUNION/creditunionindex.jpg"',
 text:"The school credit union is back running every Wednesday morning at 9.20am"}];
 
 //Once the page is loaded the following function is called
@@ -21,7 +20,7 @@ $(document).ready(function(){
 		var item = news[i]; //item is assigned to the current news story selected in the array
 		$("#newsContainer").append(newsDiv); // add newsDiv to the newsContainer on the web page
 											//and append all the news information
-		newsDiv.append('<a href='+item.link+'><h3>'+item.title+'</h3><img class = "img-responsive" src='
+		newsDiv.append('<a href="NewsAndEvents.html" '+item.link+'><h3>'+item.title+'</h3><img class = "img-responsive" src='
 		+item.image+' alt="News & Events"><h4>'+ item.text +'</h4></a><hr>');
 	}
 });
